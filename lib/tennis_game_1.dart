25db59ec228fd5a3fc3f8592e812028311e84cea
Ismail Elmogy <ismailelmogy0@gmail.com>
2025-12-27 01:53:13 +0200

    refactor: Refactor the part where player exceeds other player under four points

--- lib/tennis_game_1.dart
+++ lib/tennis_game_1.dart
@@ -26,9 +26,10 @@ class TennisGame1 implements TennisGame {
   @override
   String getScore() {
     String gameScore = "";
-    int tempScore = 0;
     if (firstPlayerScore == secondPlayerScore) {
       gameScore = checkGameScoreWhenPlayersEqual(firstPlayerScore);
+    } else if (firstPlayerScore < 4 && secondPlayerScore < 4) {
+      gameScore = "${checkPlayerScore(firstPlayerScore)}-${checkPlayerScore(secondPlayerScore)}";
     } else if (firstPlayerScore >= 4 || secondPlayerScore >= 4) {
       int minusResult = firstPlayerScore - secondPlayerScore;
       if (minusResult == 1)
@@ -39,29 +40,6 @@ class TennisGame1 implements TennisGame {
         gameScore = "Win for player1";
       else
         gameScore = "Win for player2";
-    } else {
-      for (int i = 1; i < 3; i++) {
-        if (i == 1)
-          tempScore = firstPlayerScore;
-        else {
-          gameScore += "-";
-          tempScore = secondPlayerScore;
-        }
-        switch (tempScore) {
-          case 0:
-            gameScore += "Love";
-            break;
-          case 1:
-            gameScore += "Fifteen";
-            break;
-          case 2:
-            gameScore += "Thirty";
-            break;
-          case 3:
-            gameScore += "Forty";
-            break;
-        }
-      }
     }
     return gameScore;
   }
@@ -83,5 +61,24 @@ class TennisGame1 implements TennisGame {
         break;
     }
     return resultText;
+  }
+
+  String checkPlayerScore(int score) {
+    String playerScore = "";
+    switch (score) {
+      case 0:
+        playerScore = "Love";
+        break;
+      case 1:
+        playerScore = "Fifteen";
+        break;
+      case 2:
+        playerScore += "Thirty";
+        break;
+      case 3:
+        playerScore += "Forty";
+        break;
+    }
+    return playerScore;
   }
 }
